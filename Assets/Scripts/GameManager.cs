27e648bf72d2117cccea995a899cd2270fde4713
mileyzero <64124335+mileyzero@users.noreply.github.com>
2020-06-17 16:23:52 +0800

    Updated by Jerome

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
@@ -41,5 +42,10 @@ public class GameManager : MonoBehaviour
         Time.timeScale = 0;
         Txt_Message.text = "GAMEOVER! \nPRESS ENTER TO RESTART GAME.";
         Txt_Message.color = Color.red;
+    }
+    
+    public void Restart()
+    {
+        SceneManager.LoadScene(0);
     }
 }
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,20 +1,55 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class Player : MonoBehaviour
 {
+    public static GameManager thisManager = null;
     private Animation thisAnimation;
+    public float velocity = 10;
+    public Rigidbody rb;
+    public GameObject obstacle;
+    public float addScore;
+    public Text score;
 
     void Start()
     {
         thisAnimation = GetComponent<Animation>();
         thisAnimation["Flap_Legacy"].speed = 3;
+        rb = GetComponent<Rigidbody>();
     }
 
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.Space))
+        {
             thisAnimation.Play();
+            rb.velocity = transform.up * velocity;
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag == "lose")
+        {
+            Destroy(gameObject);
+            SceneManager.LoadScene(1);
+        }
+        if(other.gameObject.name == "Collider")
+        {
+            addScore += 10;
+            score.text = "SCORE : " + addScore;
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "wall")
+        {
+            Destroy(gameObject);
+            SceneManager.LoadScene(1);
+        }
     }
 }
